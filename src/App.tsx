e48bf9206530ff6fbe9e5092fbddb18585eedda1
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-05-01 18:51:12 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,112 @@
 import { invoke } from "@tauri-apps/api/tauri";
-import React, { Children, useState } from 'react';
-import { BankOutlined, CarOutlined, DashboardOutlined, AlertOutlined, AimOutlined, ExperimentOutlined, HomeOutlined} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
-import { register } from '@tauri-apps/api/globalShortcut'
-import { writeText } from '@tauri-apps/api/clipboard';
+import React, { Children, useState } from "react";
+import {
+  BankOutlined,
+  CarOutlined,
+  DashboardOutlined,
+  AlertOutlined,
+  AimOutlined,
+  ExperimentOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import { register } from "@tauri-apps/api/globalShortcut";
+import { writeText } from "@tauri-apps/api/clipboard";
 
-register('Alt+L', async () => {
-  await invoke('show_window')
-})
+register("Alt+L", async () => {
+  await invoke("show_window");
+});
 
-const items: MenuProps['items'] = [
-   {
-      label: 'Traffic Stops',
-      key: 'SubMenu',
-      icon: <DashboardOutlined />,
-      children: [
-        {
-          label: 'Speeding Ticket',
-          key: "speedingTicket"
-          
-        },
-        {
-          label: 'Failure to obey traffic control devices',
-          key: 'controlDevices'
-        },
-        {
-         label: 'Suspended License',
-         key: 'suspendedLicense'
-        },
-        {
-         label: 'Joyriding',
-         key: 'joyriding'
-        },{
-          label: 'Negligent Driving',
-          key: 'negligentDriving'
-        }
-      ],
-    },
+const items: MenuProps["items"] = [
   {
-    label: 'Robbery of a financial institue',
-    key: 'robbery',
+    label: "Traffic Stops",
+    key: "SubMenu",
+    icon: <DashboardOutlined />,
+    children: [
+      {
+        label: "Speeding Ticket",
+        key: "speedingTicket",
+      },
+      {
+        label: "Failure to obey traffic control devices",
+        key: "controlDevices",
+      },
+      {
+        label: "Suspended License",
+        key: "suspendedLicense",
+      },
+      {
+        label: "Joyriding",
+        key: "joyriding",
+      },
+      {
+        label: "Negligent Driving",
+        key: "negligentDriving",
+      },
+    ],
+  },
+  {
+    label: "Robbery of a financial institue",
+    key: "robbery",
     icon: <BankOutlined />,
   },
   {
-    label: 'Grand Theft Auto',
-    key: 'boost',
+    label: "Grand Theft Auto",
+    key: "boost",
     icon: <CarOutlined />,
   },
   {
-    label: 'Meth/Heroin Run',
-    key: 'methRun',
+    label: "Meth/Heroin Run",
+    key: "methRun",
     icon: <AlertOutlined />,
   },
   {
-   label: 'Drug Crimes',
-   key: 'Submenu',
-   icon: <ExperimentOutlined />,
-   children: [
+    label: "Drug Crimes",
+    key: "Submenu",
+    icon: <ExperimentOutlined />,
+    children: [
       {
-        label: 'Sale of Drugs',
-        key: "saleOfDrugs"
-        
+        label: "Sale of Drugs",
+        key: "saleOfDrugs",
       },
       {
-        label: 'Felony Possession with intent to distribute',
-        key: 'intentToDistribute'
+        label: "Felony Possession with intent to distribute",
+        key: "intentToDistribute",
       },
     ],
-
   },
   {
-   label: 'Gang Related Shooting',
-   key: 'GRS',
-   icon: <AimOutlined />
+    label: "Gang Related Shooting",
+    key: "GRS",
+    icon: <AimOutlined />,
   },
   {
-   label: 'House robbery',
-   key: 'houseRobbery',
-   icon: <HomeOutlined/>
-  }
+    label: "House robbery",
+    key: "houseRobbery",
+    icon: <HomeOutlined />,
+  },
 ];
 
-
 const App: React.FC = () => {
-  const [current, setCurrent] = useState('mail');
+  const [current, setCurrent] = useState("mail");
 
-  const onClick: MenuProps['onClick'] = async (e) => {
-   await invoke('hide_window')
-   let text: string = await invoke("template",{key: e.key})
-    await writeText(text)
+  const onClick: MenuProps["onClick"] = async (e) => {
+    await invoke("hide_window");
+    let text: string = await invoke("template", { key: e.key });
+    await writeText(text);
   };
 
-  return <Menu id="menu" onClick={onClick} selectedKeys={[current]} mode="vertical" theme="dark" items={items} />;
+  return (
+    <Menu
+      id="menu"
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="vertical"
+      theme="dark"
+      items={items}
+    />
+  );
 };
 
 export default App;
