d9f911f99a976e1a242981d91c9941d597dfa1a0
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-05-02 12:32:52 +0200

    formatting

--- src-tauri/src/templates.rs
+++ src-tauri/src/templates.rs
@@ -1,38 +1,45 @@
-pub const BOOST: &str = "OFFICER responded to dispatch call about CAR being stolen at LOCATION. Upon arriving at scene OFFICER saw the CAR and attempted to pull it over. 
-SUSPECT disregarded orders to pull over and started recklessly evading by going over 100mph and driving on opposite lanes. Suspect was eventually apprehended at LOCATION 
-after resisting arrest by breaking out of cuffs. Upon further inspection there were signs of tampering in the driver side of the CAR. Car was documented and impounded.
-Suspect was charged with Grand Theft Auto, Reckless evading and Resisting arrest which they plead guilty/not guilty to.";
+pub const BOOST: &str = 
+"OFFICER responded to dispatch call about CAR being stolen at LOCATION. Upon arriving at scene OFFICER saw the CAR and attempted to pull it over. SUSPECT disregarded 
+orders to pull over and started recklessly evading by going over 100mph and driving on opposite lanes. Suspect was eventually apprehended at LOCATION after resisting 
+arrest by breaking out of cuffs. Upon further inspection there were signs of tampering in the driver side of the CAR. Car was documented and impounded. Suspect was 
+charged with Grand Theft Auto, Reckless evading and Resisting arrest which they plead guilty/not guilty to.";
 
-pub const CONTROL_DEVICES: &str = "OFFICER saw CAR going through red light(s) and attempted to pull it over. SUSPECT complied and identified themselves.
-They were give a ticket for Failure to obey traffic control devices.";
+pub const CONTROL_DEVICES: &str = 
+"OFFICER saw CAR going through red light(s) and attempted to pull it over. SUSPECT complied and identified themselves. They were give a ticket for Failure to obey traffic control devices.";
 
-pub const NEGLIGENT_DRIVING: &str = "OFFICER saw CAR going through red light(s) and going OVERSPEEDLIMIT at a SPEEDLIMIT. Officer attempted to pull the CAR over,
- SUSPECT complied and identified themselves. They were give a ticket for Negligent Driving.";
+pub const NEGLIGENT_DRIVING: &str = 
+"OFFICER saw CAR going through red light(s) and going OVERSPEEDLIMIT at a SPEEDLIMIT. Officer attempted to pull the CAR over, SUSPECT complied and identified themselves. They were give a ticket for Negligent Driving.";
 
-pub const GRS: &str = "OFFICER responded to shots fired dispatch call at LOCATION. Upon arriving at the location OFFICER found several injured people at the ground. After making sure scene is safe,
- Officer called medical on scene. OFFICER checked injured people for gunshot residue and they were positive. Injured people were part of the GANG1 and GANG2 which are on war. Suspects were charged with 
- Gang related shooting which they plead guilty/not guilty to";
+pub const GRS: &str = 
+"OFFICER responded to shots fired dispatch call at LOCATION. Upon arriving at the location OFFICER found several injured people at the ground. After making sure scene is safe,
+ Officer called medical on scene. OFFICER checked injured people for gunshot residue and they were positive. Injured people were part of the GANG1 and GANG2 which are on war. 
+ Suspects were charged with Gang related shooting which they plead guilty/not guilty to.";
 
-pub const HOUSE_ROBBERY: &str = "OFFICER responded to a house alarm at LOCATION. Upon arriving at the area OFFICER saw SUSPECT and attempted to detain him for questioning. SUSPECT tried to flee on foot/with CAR 
-and was eventually apprehended after resisting arrest by trying to break cuffs. SUSPECT had scratches on their hands and their DNA matched with the blood on the door of the house that was being robbed they were 
-also in Possession of stolen goods from the house. Suspect was charged with Robbery, Resisting Arrest, Possession of third degree stolen goods which they plead guilty/ not guilty to.";
+pub const HOUSE_ROBBERY: &str = 
+"OFFICER responded to a house alarm at LOCATION. Upon arriving at the area OFFICER saw SUSPECT and attempted to detain him for questioning. SUSPECT tried to flee on foot/with CAR 
+and was eventually apprehended after resisting arrest by trying to break cuffs. SUSPECT had scratches on their hands and their DNA matched with the blood on the door of the house
+ that was being robbed they were also in Possession of stolen goods from the house. Suspect was charged with Robbery, Resisting Arrest, Possession of third degree stolen goods 
+ which they plead guilty/ not guilty to.";
 
-pub const METH_RUN: &str = "OFFICER responded to dispatch call about an armored call. OFFICER intercepred the car at LOCATION and attempted the pull it over. SUSPECT didn't comply and started to recklessly evade by 
-going over 100 mph and driving on opposite lanes. Car was boxed in/broken at LOCATION, SUSPECT get out of the car and started to flee on foot. After resisting arrest by trying to break cuffs SUSPECT was eventually 
-apprehended. SUSPECT was charged with Reckless Evading and Resisting Arrest which they plead guilty/not guilty to.";
+pub const METH_RUN: &str = 
+"OFFICER responded to dispatch call about an armored call. OFFICER intercepred the car at LOCATION and attempted the pull it over. SUSPECT didn't comply and started to recklessly 
+evade by going over 100 mph and driving on opposite lanes. Car was boxed in/broken at LOCATION, SUSPECT get out of the car and started to flee on foot. After resisting arrest by 
+trying to break cuffs SUSPECT was eventually apprehended. SUSPECT was charged with Reckless Evading and Resisting Arrest which they plead guilty/not guilty to.";
 
-pub const BANK: &str = "OFFICER responded to a bank alarm. Upon arriving at the bank saw masked people inside the bank holding a hostage. OFFICER initiated negotiatons for hostages release.
- Suspects agreed to release to hostage for safe passage to their car which was parked in front of the bank. After receiving the hostage officers start pursuing the car. SUSPECT(s) was/were 
- eventually apprenhended after recklessly evading by going over 100 mph and driving on the opposite lanes and resisitng arrest. SUSPECT was in possession of stolen money from the bank which 
- was seized. SUSPECT was charged with Robbery of a Financial Institue which they plead guilty/not gulty to.";
+pub const BANK: &str = 
+"OFFICER responded to a bank alarm. Upon arriving at the bank saw masked people inside the bank holding a hostage. OFFICER initiated negotiatons for hostages release. Suspects agreed 
+to release to hostage for safe passage to their car which was parked in front of the bank. After receiving the hostage officers start pursuing the car. SUSPECT(s) was/were eventually 
+apprenhended after recklessly evading by going over 100 mph and driving on the opposite lanes and resisitng arrest. SUSPECT was in possession of stolen money from the bank which was 
+seized. SUSPECT was charged with Robbery of a Financial Institue which they plead guilty/not gulty to.";
 
-pub const SALE_OF_DRUGS: &str = "OFFICER responded to a suspicious activity dispatch call at LOCATION. Upon arriving at location OFFICER saw suspect and started to watch them. During that time OFFICER saw 
-SUSPECT do several hand offs. OFFICER moved in to detain SUSPECT for questioning but SUSPECT got on VEHICLE and fled. SUSPECT was eventaully apprehended after recklessly evading by going over 100 mph and 
-driving on opposite lanes and resisting arrest by breaking cuffs. When searched SUSPECT was in possesion of AMOUNT cash, AMOUNT DRUG and AMOUNT dirty money. SUSPECT was charged with Sale of Drugs, reckless 
-evading and resisting arrest which they plead guilty/not guilty to.";
+pub const SALE_OF_DRUGS: &str = 
+"OFFICER responded to a suspicious activity dispatch call at LOCATION. Upon arriving at location OFFICER saw suspect and started to watch them. During that time OFFICER saw SUSPECT do 
+several hand offs. OFFICER moved in to detain SUSPECT for questioning but SUSPECT got on VEHICLE and fled. SUSPECT was eventaully apprehended after recklessly evading by going over 100 
+mph and driving on opposite lanes and resisting arrest by breaking cuffs. When searched SUSPECT was in possesion of AMOUNT cash, AMOUNT DRUG and AMOUNT dirty money. SUSPECT was charged 
+with Sale of Drugs, reckless evading and resisting arrest which they plead guilty/not guilty to.";
 
-pub const SPEEDING_TICKET: &str = "OFFICER clocked CAR going OVERSPEEDLIMIT on a SPEEDLIMIT. SUSPECT complied after being told to pull over and identified themselves.
-SUSPECT was issued a third/second/first degree speeding ticket.";
+pub const SPEEDING_TICKET: &str = 
+"OFFICER clocked CAR going OVERSPEEDLIMIT on a SPEEDLIMIT. SUSPECT complied after being told to pull over and identified themselves. SUSPECT was issued a third/second/first degree speeding ticket.";
 
-pub const SUSPENDED_LICENSE: &str = "OFFCER pulled over CAR in LOCATION for REASON. SUSPECT complied and identified themselves however their license was suspended.Driving without a license
-SUSPECT was arrested for operating a vehicle with a suspended license and their car was impounded.";
+pub const SUSPENDED_LICENSE: &str = 
+"OFFCER pulled over CAR in LOCATION for REASON. SUSPECT complied and identified themselves however their license was suspended. SUSPECT was arrested for operating a vehicle with a suspended license and their car was impounded.";
